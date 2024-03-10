9ff80f2169d76f8339dabce40aec7e77d4c78f03
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-03-11 01:32:23 +0530

    layout exploreing

---
+++ app/auth/layout.tsx
@@ -0,0 +1,15 @@
+import React  from 'react'
+
+export default function({ children }: {
+    children : React.ReactNode
+}) {
+  return (
+    <div>
+
+    <div className='border-b text-center'>
+        20 % off Banner
+    </div>
+        {children}
+    </div>
+  )
+}
